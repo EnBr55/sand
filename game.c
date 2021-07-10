4a437debe4c43cd33723d7f04e75d8b74c32190e
Ben <bjbraham@gmail.com>
2021-07-11 00:28:16 +1000

    Write client driver

--- game.c
+++ game.c
@@ -1,15 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <GL/glew.h>
 #include <GL/freeglut.h>
 #include "renderer.h"
 
-int main(int argc, char* argv[]) {
-  Initialize(argc, argv);
+SimData sim;
 
-  glutMainLoop();
-  
-  exit(EXIT_SUCCESS);
+float * Init() {
+  printf("Sim initializing...\n");
+  // floats for RGBA vals of each cell
+  sim.cellColors = malloc(sizeof(float[4]) * NUM_CELLS);
+  float * cellColors = sim.cellColors;
+  for (int i = 0; i < NUM_CELLS; i++) {
+    cellColors[i*RGBA_OFFSET + 0] = 0.1f;
+    cellColors[i*RGBA_OFFSET + 1] = 0.0f;
+    cellColors[i*RGBA_OFFSET + 2] = 0 *(i * 1.0f) / NUM_CELLS + 0.0f;
+    cellColors[i*RGBA_OFFSET + 3] = 1.0f;
+  }
+  return cellColors;
 }
 
+float * Render() {
+  float * cellColors = sim.cellColors;
+  return cellColors;
+}
+
+void CleanupSim() {
+  free(sim.cellColors);
+}
+
+int main(int argc, char* argv[]) {
+  Initialize(argc, argv);
+  glutMainLoop();
+  return 0;
+}
